b40f55f962880d077ec8c02950ab40e0c0e794ed
Nicolai Willems <172633+nwillems@users.noreply.github.com>
2019-05-06 14:42:05 +0200

    Ensure error-free DB creation

--- OracleDatabase/SingleInstance/dockerfiles/12.2.0.1/createDB.sh
+++ OracleDatabase/SingleInstance/dockerfiles/12.2.0.1/createDB.sh
@@ -74,13 +74,15 @@ echo "$ORACLE_PDB=
   )
 )" >> $ORACLE_HOME/network/admin/tnsnames.ora
 
+# COMMENTED - Only allowed on container DB
 # Remove second control file, fix local_listener, make PDB auto open, enable EM global port
-sqlplus / as sysdba << EOF
-   ALTER SYSTEM SET control_files='$ORACLE_BASE/oradata/$ORACLE_SID/control01.ctl' scope=spfile;
-   ALTER PLUGGABLE DATABASE $ORACLE_PDB SAVE STATE;
-   EXEC DBMS_XDB_CONFIG.SETGLOBALPORTENABLED (TRUE);
-   exit;
-EOF
+#sqlplus / as sysdba << EOF
+#   ALTER SYSTEM SET control_files='$ORACLE_BASE/oradata/$ORACLE_SID/control01.ctl' scope=spfile;
+#   ALTER PLUGGABLE DATABASE $ORACLE_PDB SAVE STATE;
+#   EXEC DBMS_XDB_CONFIG.SETGLOBALPORTENABLED (TRUE);
+#   exit;
+#EOF
 
 # Remove temporary response file
+#
 rm $ORACLE_BASE/dbca.rsp
